c35e3304776e3edd4b87042e7317ecc751066582
Michael-Mankiewicz <mankiewiczmichael@gmail.com>
2024-03-01 15:26:13 -0500

    cd/cd test

--- src/components/ScriptCard.tsx
+++ src/components/ScriptCard.tsx
@@ -73,7 +73,7 @@ function ScriptCard({}) {
             <Card sx={{display: "flex", justifyContent: "center"}}>
             <Box sx={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
                 <h1>Address Correction Script</h1>
-                <h3>This script creates bill invoices containing all Fedex address changes per project!</h3>
+                <h3>This script creates bill invoices containing Fedex address changes per project!</h3>
                 
                 <form action="" onSubmit={handleSubmit(onSubmit) }>
                     <Box sx={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
